3c2a692091fc17155499f35a76ebea29af9c99d5
AngryCyboot <cyboot25@gmail.com>
2016-09-15 23:21:47 +0200

    correction murs + anim soldat
    ajout d'un mesh pour les murs
    randomisation des anim du soldat

---
+++ Jeu 0 - Demo/Assets/Scripts/randomBoolGenerator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class randomBoolGenerator : MonoBehaviour
+{
+    private Animator anim;
+
+    public void Update()
+    {
+        anim = this.GetComponent<Animator>();
+        Debug.Log("fqsd");
+        if (Random.Range(0,2) == 1)
+        {
+            anim.SetBool("randomBool", true);
+            Debug.Log("1");
+        }
+        else
+        {
+            anim.SetBool("randomBool", false);
+            Debug.Log("0");
+        }
+    }
+}
