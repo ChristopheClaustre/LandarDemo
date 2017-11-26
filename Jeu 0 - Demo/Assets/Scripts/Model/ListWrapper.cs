e307285d9711b3815ffc866a1f73f527144343b7
ChristopheClaustre <christophe.claustre.31@gmail.com>
2017-11-26 22:00:28 +0100

    [FCT] classes de gestion de listes d'éléments

---
+++ Jeu 0 - Demo/Assets/Scripts/Model/ListWrapper.cs
@@ -0,0 +1,149 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+[System.Serializable]
+public class ListWrapper<T>
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    /********  INSPECTOR        ************************/
+
+    [SerializeField] protected List<T> m_list;
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+    
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    // Contructor
+    public ListWrapper() : this(new List<T>())
+    {
+    }
+    // Contructor with parameter
+    public ListWrapper(List<T> p_init)
+    {
+        m_list = p_init;
+    }
+
+    // Getter by indice
+    public virtual T At(int p_indice)
+    {
+        return m_list[p_indice];
+    }
+
+    // Add
+    public virtual ListWrapper<T> Add(T p_element)
+    {
+        m_list.Add(p_element);
+
+        return this;
+    }
+
+    public virtual ListWrapper<T> AddRange(IEnumerable<T> p_enumerable)
+    {
+        m_list.AddRange(p_enumerable);
+
+        return this;
+    }
+    public virtual ListWrapper<T> AddRange(ListWrapper<T> p_listWrapper)
+    {
+        return AddRange(p_listWrapper.m_list);
+    }
+
+    // Remove
+    public virtual ListWrapper<T> Remove(T p_element)
+    {
+        m_list.Remove(p_element);
+
+        return this;
+    }
+
+    public virtual ListWrapper<T> RemoveRange(IEnumerable<T> p_enumerable)
+    {
+        foreach (T t in p_enumerable)
+        {
+            if (m_list.Contains(t))
+            {
+                Remove(t);
+            }
+        }
+        //AddRange(p_enumerable);
+
+        return this;
+    }
+    public virtual ListWrapper<T> RemoveRange(ListWrapper<T> p_listWrapper)
+    {
+        return RemoveRange(p_listWrapper.m_list);
+    }
+
+    // Clear
+    public virtual ListWrapper<T> Clear()
+    {
+        m_list.Clear();
+
+        return this;
+    }
+
+    /********  PROTECTED        ************************/
+
+    // Getter by type
+    protected virtual List<U> GetFromType<U>() where U : T
+    {
+        return m_list.FindAll(t => t is U) as List<U>;
+    }
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
---
+++ Jeu 0 - Demo/Assets/Scripts/Model/PersonnageList.cs
@@ -0,0 +1,67 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+[System.Serializable]
+public class PersonnageList :
+    ListWrapper<Personnage>
+{
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    public List<Personnage> Personnages
+    {
+        get
+        {
+            return new List<Personnage>(m_list);
+        }
+    }
+
+    /********  PROTECTED        ************************/
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    /********  PROTECTED        ************************/
+    
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    public PersonnageList() : base( new List<Personnage>() )
+    {
+    }
+
+    public int GetConsumptionFood()
+    {
+        return 0; // TODO
+    }
+
+    // TODO : d'autres fonctions ...
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
