eaac46328aae708ce82374bfbaf53046c3598fb8
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-01-08 22:16:32 +0100

    [FCT] model Task script

---
+++ Jeu 0 - Demo/Assets/Scripts/Model/Task/Goto.cs
@@ -0,0 +1,100 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class Goto : Activity
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    private enum State
+    {
+        eBegin,
+        eMoving,
+        eFinished
+    }
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    private Vector3 m_localisation;
+    private float m_orientation;
+    private State m_state;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public Goto(Vector3 p_localisation, float p_orientation = float.NaN)
+    {
+        m_localisation = p_localisation;
+        m_orientation = p_orientation;
+    }
+
+    public override bool DoAction(PersonnageScript p_personnageScript)
+    {
+        bool lActionFinished = false;
+        switch(m_state)
+        {
+            case State.eBegin:
+                Goto(p_personnageScript);
+                m_state = State.eMoving;
+                lActionFinished = false;
+                break;
+            case State.eMoving:
+                if (GotoFinished(p_personnageScript))
+                {
+                    m_state = State.eFinished;
+                    lActionFinished = true;
+                }
+                else
+                {
+                    lActionFinished = false;
+                }
+                break;
+            case State.eFinished:
+                lActionFinished = true;
+                break;
+        }
+
+        return lActionFinished;
+    }
+
+    public override Vector3 GetLocalisation()
+    {
+        return m_localisation;
+    }
+
+    public override float GetOrientation()
+    {
+        return m_orientation;
+    }
+
+    public override bool HasOrientation()
+    {
+        return float.IsNaN(m_orientation);
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
---
+++ Jeu 0 - Demo/Assets/Scripts/Model/Task/Task.cs
@@ -0,0 +1,55 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public abstract class Activity
+{
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    public Activity() {}
+
+    public virtual bool CheckAction() { return true; }
+    public abstract bool DoAction(PersonnageScript p_personnageScript);
+    public abstract Vector3 GetLocalisation();
+
+    public virtual bool HasOrientation() { return false; }
+    public virtual float GetOrientation() { return float.NaN; }
+
+    public void Goto(PersonnageScript p_personnageScript)
+    {
+        // @todo
+    }
+
+    public bool GotoFinished(PersonnageScript p_personnageScript)
+    {
+        // @todo
+        return true;
+    }
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
