160b182d7a9fa56dd14ceb0b183811228e3953ae
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-01-08 22:01:05 +0100

    [FCT] a fucking script to add the slowmotion mode

---
+++ Jeu 0 - Demo/Assets/Scripts/Expedition/PauseTime.cs
@@ -0,0 +1,91 @@
+﻿/***************************************************/
+/***  INCLUDE               ************************/
+/***************************************************/
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+/***************************************************/
+/***  THE CLASS             ************************/
+/***************************************************/
+public class PauseTime :
+    MonoBehaviour
+{
+    #region Sub-classes/enum
+    /***************************************************/
+    /***  SUB-CLASSES/ENUM      ************************/
+    /***************************************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+    #region Property
+    /***************************************************/
+    /***  PROPERTY              ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Constants
+    /***************************************************/
+    /***  CONSTANTS             ************************/
+    /***************************************************/
+
+
+
+    #endregion
+    #region Attributes
+    /***************************************************/
+    /***  ATTRIBUTES            ************************/
+    /***************************************************/
+
+    public bool m_pauseTime = false;
+
+    #endregion
+    #region Methods
+    /***************************************************/
+    /***  METHODS               ************************/
+    /***************************************************/
+
+    /********  UNITY MESSAGES   ************************/
+
+    // Use this for initialization
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_pauseTime = !m_pauseTime;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Time.timeScale = m_pauseTime ? 0.05f : 1;
+    }
+
+    private void OnGUI()
+    {
+        m_pauseTime = GUILayout.Toggle(m_pauseTime, m_pauseTime ? ">" : "||");
+    }
+
+    /********  OUR MESSAGES     ************************/
+
+    /********  PUBLIC           ************************/
+
+    /********  PROTECTED        ************************/
+
+    /********  PRIVATE          ************************/
+
+    #endregion
+}
