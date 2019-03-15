207b9bd6c792d56930302cea9e0af4ba12ef8c17
ChristopheClaustre <christophe.claustre.31@gmail.com>
2019-03-15 02:29:20 +0100

    [FCT] new and better UserControl for Character

---
+++ Jeu 0 - Demo/Assets/XAML/Expedition/CharacterUC.xaml.cs
@@ -0,0 +1,45 @@
+﻿#if UNITY_5_3_OR_NEWER
+#define NOESIS
+using Noesis;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+#else
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+#endif
+
+namespace Noesis
+{
+    /// <summary>
+    /// Logique d'interaction pour CharacterUC.xaml
+    /// </summary>
+    public partial class CharacterUC : UserControl
+    {
+        public CharacterUC()
+        {
+            InitializeComponent();
+        }
+
+#if NOESIS
+		private void InitializeComponent()
+		{
+			Noesis.GUI.LoadComponent(this, "Assets/XAML/Expedition/CharacterUC.xaml");
+		}
+#endif
+    }
+}
