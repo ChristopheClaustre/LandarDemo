4de0a7c3b2a57cf55dc4f1d9bafdebef29804871
AngryCyboot <cyboot25@gmail.com>
2019-07-21 18:03:24 +0200

    [WIP] test tir progress

---
+++ Jeu 0 - Demo/Assets/Scripts/TirAnim.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TirAnim : MonoBehaviour
+{
+	[SerializeField]
+	private GameObject Projectile;
+	
+	void bang ()
+	{
+		Instantiate(Projectile,transform.position,transform.rotation);
+	}
+}
